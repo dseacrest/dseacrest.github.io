a1470593b7789a84a2027f73a405633dad23603f
dseacrest <drseacrest@gmail.com>
2019-12-26 16:32:27 -0600

    Config

--- shared-checklists/babel.config.js
+++ shared-checklists/babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+  presets: ["@vue/cli-plugin-babel/preset"],
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/dseacrest.github.io/'
+  : '/'
 };
