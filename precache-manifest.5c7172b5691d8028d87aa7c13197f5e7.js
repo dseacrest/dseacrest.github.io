d57a42f473f6cd41d2c583c61e7781c61a2b7837
dseacrest <drseacrest@gmail.com>
2020-04-06 22:34:12 -0500

    deploy 32

---
+++ precache-manifest.5c7172b5691d8028d87aa7c13197f5e7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c989aea7e9587285693869ce1db13c2",
+    "url": "/dseacrest.github.io/CNAME"
+  },
+  {
+    "revision": "de4b7eb34f33699fe5ec",
+    "url": "/dseacrest.github.io/css/app.07b5f8fb.css"
+  },
+  {
+    "revision": "958a3e4c75b3fb05ac26",
+    "url": "/dseacrest.github.io/css/chunk-vendors.916c14b9.css"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/dseacrest.github.io/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/dseacrest.github.io/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "1499a1eac6be3f493d12bf249ca67274",
+    "url": "/dseacrest.github.io/img/header.1499a1ea.png"
+  },
+  {
+    "revision": "b32308afc8678fb1683d18d15ad6ea30",
+    "url": "/dseacrest.github.io/img/logo-mobile.b32308af.png"
+  },
+  {
+    "revision": "60b98ed0a77932e2cc800a4d4b9d585d",
+    "url": "/dseacrest.github.io/index.html"
+  },
+  {
+    "revision": "de4b7eb34f33699fe5ec",
+    "url": "/dseacrest.github.io/js/app.075695d8.js"
+  },
+  {
+    "revision": "958a3e4c75b3fb05ac26",
+    "url": "/dseacrest.github.io/js/chunk-vendors.c3ea9606.js"
+  },
+  {
+    "revision": "ec41a0036c2867aa1e75b78d99b87da5",
+    "url": "/dseacrest.github.io/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/dseacrest.github.io/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/dseacrest.github.io/precache-manifest.5c7172b5691d8028d87aa7c13197f5e7.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "shared-checklists"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
